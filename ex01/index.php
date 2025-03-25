e21e4f50b0f176782d851912715acc85253107c1
NetoPetit <cidao_petit5@hotmail.com>
2025-03-25 13:10:33 -0300

    inicio desafio curso

---
+++ ex01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Ola mundo! \u{1F30E} \u{1F680}";
+        ?>
+    </h1>
+    <p>Ola mundo</p>
+</body>
+</html>
