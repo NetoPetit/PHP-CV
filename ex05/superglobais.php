62a357717a1a878402f7263c67de43023033c3bd
NetoPetit <cidao_petit5@hotmail.com>
2025-03-29 16:26:06 -0300

    superglobais

---
+++ ex05/superglobais.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobais</title>
+    <link rel="stylesheet" href="../desafios_php/style.css">
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+                echo "<h1>Superglobal GET</h1>";
+                var_dump($_GET);
+                //http://localhost/PHP_CV/ex05/superglobais.php?nome=Neto&idade=36 (passando dados pro array através da URL)
+
+                echo "<h1>Superglobal POST</h1>";
+                var_dump($_POST);
+            ?>
+        </pre>
+    </main>
+</body>
+</html>
