7cc7fdf0eb679bb05334dc077de594287b9806b4
NetoPetit <cidao_petit5@hotmail.com>
2025-03-31 18:00:04 -0300

    superglobal

--- ex05/superglobais.php
+++ ex05/superglobais.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +10,11 @@
     <main>
         <pre>
             <?php 
+                setcookie("dia-da-semana", "SEGUNDA", time() + 3600);
+
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU";
+
                 echo "<h1>Superglobal GET</h1>";
                 var_dump($_GET);
                 //http://localhost/PHP_CV/ex05/superglobais.php?nome=Neto&idade=36 (passando dados pro array através da URL)
@@ -20,6 +25,24 @@
                 //Superglobal REQUEST pega o GET e o POST
                 echo "<h1>Superglobal REQUEST</h1>";
                 var_dump($_REQUEST);
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+                /*foreach(getenv() as $c => $v){
+                    echo "<br>$c -> $v";
+                }*/
+
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
     </main>
