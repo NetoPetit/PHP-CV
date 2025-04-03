3e664e23a042d3efa84c86fad7833c542401b987
NetoPetit <cidao_petit5@hotmail.com>
2025-04-03 18:07:41 -0300

    atualização desafios

--- desafios_php/minha_resolucao/desafio07/index.php
+++ desafios_php/minha_resolucao/desafio07/index.php
@@ -9,9 +9,9 @@
 <body>
     <main>
         <h1>Informe seu Salário</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="salario">Salário(R$)</label>
-            <input type="number" name="salario" id="salario" value="">
+            <input type="number" name="salario" id="salario">
             <p>Considerando o salário mínimo de <strong>R$1.500,00</strong></p>
             <input type="submit" value="Calcular">
         </form>
@@ -19,7 +19,14 @@
     <section>
         <h2>Resultado Final</h2>
         <?php 
-            echo "<p>Quem recebe um salário de R$...</p>"
+            $salario = $_GET['salario'] ?? 0;
+            $numeros = range(0, $salario);
+            $quantidade = 0;
+            foreach($numeros as $numero){
+                $quantidade += 1;
+            }
+            echo "qtd => $quantidade";
+            echo "<p>Quem recebe um salário de R$" . number_format($salario, 2, ',', '.') . "ganha...</p>";
         ?>
     </section>
 </body>
