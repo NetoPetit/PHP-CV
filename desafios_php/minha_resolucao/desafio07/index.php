f70e0c95060067b0a06a507fc38ae8b5623b6b71
NetoPetit <cidao_petit5@hotmail.com>
2025-04-05 15:45:00 -0300

    desafio 07

--- desafios_php/minha_resolucao/desafio07/index.php
+++ desafios_php/minha_resolucao/desafio07/index.php
@@ -12,21 +12,18 @@
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="salario">Salário(R$)</label>
             <input type="number" name="salario" id="salario">
-            <p>Considerando o salário mínimo de <strong>R$1.500,00</strong></p>
+            <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <h2>Resultado Final</h2>
         <?php 
-            $salario = $_GET['salario'] ?? 0;
-            $numeros = range(0, $salario);
-            $quantidade = 0;
-            foreach($numeros as $numero){
-                $quantidade += 1;
-            }
-            echo "qtd => $quantidade";
-            echo "<p>Quem recebe um salário de R$" . number_format($salario, 2, ',', '.') . "ganha...</p>";
+            $seuSalario = $_GET['salario'] ?? 0;
+            $salarioMinimo = 1380;
+            $quociente = (int)($seuSalario / $salarioMinimo);
+            $resto = $seuSalario % $salarioMinimo;
+            echo "<p>Quem recebe um salário de R$" . number_format($seuSalario, 2, ',', '.') . " ganha <strong>$quociente salários mínimos</strong> + R$ " . number_format($resto, 2, ',', '.') . ".</p>";
         ?>
     </section>
 </body>
