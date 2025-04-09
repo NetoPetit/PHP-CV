69702f3662f0ca77e0ee23ce96d6cd12585dd70e
NetoPetit <cidao_petit5@hotmail.com>
2025-04-09 17:39:37 -0300

    desafio 11

--- desafios_php/minha_resolucao/desafio11/index.php
+++ desafios_php/minha_resolucao/desafio11/index.php
@@ -8,18 +8,35 @@
 </head>
 <body>
     <?php 
-        $valor = $_GET['v'] ?? 0;
+        $preco = $_GET['preco'] ?? 1;
+        $porcentagem = $_GET['porcentagem'] ?? 0;
     ?>
     <main>
+        <h1>Reajustador de Preços</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <input type="range" name="v" id="v" min="0" max="100" value="<?=$valor?>">
-            <input type="submit" value="Calcular">
+            <label for="preco">Preço do Produto (R$)</label>
+            <input type="number" name="preco" id="preco" min="1" value="<?=$preco?>">
+            <label for="porcentagem">Qual será o percentual do reajuste<strong><p id="valor"></p>%</strong></label>
+            <input type="range" name="porcentagem" id="porcentagem" min="0" max="100" value="<?=$porcentagem?>">
+            <input type="submit" value="Reajustar">
         </form>
     </main>
     <section>
-        <?php 
-            echo "$valor";
+        <?php
+            $reajuste = ($porcentagem * $preco) / 100; 
+            $valorAtual = $preco + $reajuste;
         ?>
+        <h2>Resultado do Reajuste</h2>
+        <p>O produto que custava R$<?=number_format($preco, 2, ',', '.')?>, com <strong><?=$porcentagem?>% de aumento</strong> vai passar a custar <strong>R$<?=number_format($valorAtual, 2, ',', '.')?></strong> a partir de agora.</p>
     </section>
+    <script>
+        const range = document.getElementById("porcentagem");
+        const valor = document.getElementById("valor");
+
+        valor.textContent = range.value;
+        range.addEventListener("input", function() {
+            valor.textContent = range.value;
+        });
+    </script>
 </body>
 </html>
