9e343ac394bcfab78058b2ca7d9142563dda2c17
NetoPetit <cidao_petit5@hotmail.com>
2025-04-13 15:51:22 -0300

    desafio 12

--- desafios_php/minha_resolucao/desafio12/index.php
+++ desafios_php/minha_resolucao/desafio12/index.php
@@ -8,24 +8,37 @@
 </head>
 <body>
     <?php 
-        $seg = $_GET['seg'] ?? 0;
+        $totSeg = $_GET['totSeg'] ?? 0;
     ?>
     <main>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="seg">Qual é o total de segundos?</label>
-            <input type="number" name="seg" id="seg" step="0.01" value="<?=$seg?>">
+            <label for="totSeg">Qual é o total de segundos?</label>
+            <input type="number" name="totSeg" id="totSeg" step="0.01" value="<?=$totSeg?>">
             <input type="submit" value="Calcular">
         </form>
     </main>
+    <?php 
+        $semana = 604_800;
+        $dia = 86_400;
+        $hora = 3_600;
+        $min = 60;
+        $seg = 1;
+
+        $qtdSemana = (int)($totSeg / $semana);
+        $qtdDia = (int)($totSeg / $dia);
+        $qtdHora = (int)($totSeg / $hora);
+        $qtdMin = (int)($totSeg / $min);
+        $qtdSegundo = (int)($totSeg / $seg);
+    ?>
     <section>
-        <p>Analisando o valor que você digitou, <strong><?=number_format($seg, 3, ',', '.')?> segundos </strong>equivalem a um total de:</p>
+        <p>Analisando o valor que você digitou, <strong><?=number_format($totSeg, 3, ',', '.')?> segundos </strong>equivalem a um total de:</p>
         <ul>
-            <li>semanas</li>
-            <li>dias</li>
-            <li>horas</li>
-            <li>minutos</li>
-            <li>segundos</li>
+            <li><?=$qtdSemana?> semanas</li>
+            <li><?=$qtdDia?> dias</li>
+            <li><?=$qtdHora?> horas</li>
+            <li><?=$qtdMin?> minutos</li>
+            <li><?=$qtdSegundo?> segundos</li>
         </ul>
     </section>
 </body>
